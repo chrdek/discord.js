43f35b390833b3b67a06b6554dbeedaaa696f18a
chrdek <cdekatrr@hotmail.com>
2021-10-15 21:29:49 +0300

    Update ApplicationCommand.js

--- src/structures/ApplicationCommand.js
+++ src/structures/ApplicationCommand.js
@@ -200,8 +200,6 @@ class ApplicationCommand extends Base {
       ('description' in command && command.description !== this.description) ||
       ('version' in command && command.version !== this.version) ||
       (commandType && commandType !== this.type) ||
-      // Future proof for options being nullable
-      // TODO: remove ?? 0 on each when nullable
       (command.options?.length ?? 0) !== (this.options?.length ?? 0) ||
       (command.defaultPermission ?? command.default_permission ?? true) !== this.defaultPermission
     ) {
